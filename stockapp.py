b4b41ad744e26ca570757f84e3c7d874a15b2337
Georges Elkassouf <georges.elkassouf@gmail.com>
2022-10-27 16:34:40 +0100

    Update stockapp.py

--- stockapp.py
+++ stockapp.py
@@ -73,26 +73,36 @@ def stockretrieve(start_date, end_date):
 
      try:
            string_logo = '<img src=%s>' % tickerData.info['logo_url']
-           st.markdown(string_logo, unsafe_allow_html=True)
-
-           string_name =tickerData.info['longName']
-           st.header('**%s**' % string_name)
-
-           st.markdown('#### Summary')
-           string_summary = tickerData.info['longBusinessSummary']
-           st.info(string_summary)
-
-           st.markdown('#### Industry')
-           string_industry = tickerData.info['industry']
-           st.info(string_industry)
-
-           st.markdown('#### Country')
-           string_country = tickerData.info['country']
-           st.info(string_country)
-      
+           st.markdown(string_logo, unsafe_allow_html=True)   
      except:
            pass
       
+      try:
+           string_name =tickerData.info['longName']
+           st.header('**%s**' % string_name)       
+      except:
+           pass
+
+      try:
+           st.markdown('#### Summary')
+           string_summary = tickerData.info['longBusinessSummary']
+           st.info(string_summary)
+      except:
+           pass
+            
+      try:           
+           st.markdown('#### Industry')
+           string_industry = tickerData.info['industry']
+           st.info(string_industry)
+      except:
+           pass
+
+      try:
+           st.markdown('#### Country')
+           string_country = tickerData.info['country']
+           st.info(string_country)
+      except:
+           pass
 
      # Ticker Data
      st.header('**Data**')
